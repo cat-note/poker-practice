e82e85b62efd607419f938d573d94cc1e85f0bc7
soeur <juzi201314@gmail.com>
2021-09-29 23:21:37 +0800

    add rs

--- poker-cl/src/main.lisp
+++ poker-cl/src/main.lisp
@@ -2,4 +2,9 @@
   (:use :cl))
 (in-package :poker-cl)
 
-;; blah blah blah.
+(defun gen_poker (num) (mapcar (lambda (tag) (format nil "~S~S" tag num)) (list "黑桃" "红心" "梅花" "街砖")))
+
+(defun gen_two_poker ()
+  (mapcar 'gen_poker (list 2 3 4 5 6 7 8 9 10 'j 'q 'k 'a)))
+
+(mapcar 'print (gen_two_poker))
